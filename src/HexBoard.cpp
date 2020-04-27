ad9c60c3cdd1d9d545bf6df804764ab7e9e412c2
JorgeAAH <georgelemetra@gmail.com>
2020-04-26 19:44:55 -0600

    Coded class MatrixSolver
    Coded class MatrixSolver to accelerate the solving of matrix in evaluation.
    MatrixSolver implements the iterative conjugate gradient method until the error is reduced below a threshold or a security stopping condition is reached.

--- src/HexBoard.cpp
+++ src/HexBoard.cpp
@@ -31,11 +31,17 @@ class UnionFindData {
 
 class MatrixSolver {
     public:
-        MatrixSolver(uint16_t number_of_unknowns);
+        MatrixSolver(uint16_t number_of_unknowns_parameter);
         ~MatrixSolver();
+        uint16_t add_assignement(uint16_t row, uint16_t column, float add_assignement_value);
+        float *solve_matrix(float *b_column_matrix);
 
     private:
-
+        float *multiply_sparse_matrix_with_column_matrix(float *column_matrix);
+        uint16_t number_of_unknowns;
+        uint16_t *row_occupation;
+        uint16_t **matrix_column_positions;
+        float **matrix_values;
 };
 
 GeneratedMoves::GeneratedMoves(uint16_t number_of_hexes){
@@ -166,6 +172,141 @@ uint16_t UnionFindData::find_parent(uint16_t id_a){
     }
     delete []to_be_compressed;
     return parent_a_node;
+}
+
+MatrixSolver::MatrixSolver(uint16_t number_of_unknowns_parameter){
+    number_of_unknowns = number_of_unknowns_parameter;
+    row_occupation = new uint16_t[number_of_unknowns];
+    matrix_column_positions = new uint16_t*[number_of_unknowns];
+    matrix_values = new float*[number_of_unknowns];
+    for (uint16_t i = 0; i < number_of_unknowns; i++){
+        row_occupation[i] = 0;
+        //We only need 7 places: 1 for the central hexagon and 6 for the adjacent.
+        matrix_column_positions[i] = new uint16_t[7];
+        matrix_values[i] = new float[7];
+    }
+}
+
+MatrixSolver::~MatrixSolver(){
+    delete [] row_occupation;
+    for (uint16_t i = 0; i < number_of_unknowns; i++){
+        delete [] matrix_column_positions[i];
+        delete [] matrix_values[i];
+    }
+    delete [] matrix_column_positions;
+    delete [] matrix_values;
+}
+
+uint16_t MatrixSolver::add_assignement(uint16_t row, uint16_t column, float add_assignement_value){
+    uint16_t actual_row_occupation = row_occupation[row];
+    for (uint16_t i = 0; i < actual_row_occupation; i++){
+        if (column == matrix_column_positions[row][i]){
+            //The element exist on the sparse matrix, so we only add-assign.
+            matrix_values[row][i] += add_assignement_value;
+            return 0;
+        }
+    }
+    //We create the new element of the sparse matrix.
+    matrix_column_positions[row][actual_row_occupation] = column;
+    matrix_values[row][actual_row_occupation] = add_assignement_value;
+    row_occupation[row]++;
+    return 0;
+}
+
+float *MatrixSolver::solve_matrix(float *b_column_matrix){
+    //We use the following implementation of conjugate gradient descent (from Wikipedia):
+    //r_0 = b - Ax_0
+    float *actual_x = new float[number_of_unknowns];
+    for (uint16_t i = 0; i < number_of_unknowns; i++){
+        actual_x[i] = 0.0F;
+    }
+    float *actual_r = multiply_sparse_matrix_with_column_matrix(actual_x);
+    for (uint16_t i = 0; i < number_of_unknowns; i++){
+        actual_r[i] = b_column_matrix[i] - actual_r[i];
+    }
+    //p_0 = r_0
+    float *actual_p = new float[number_of_unknowns];
+    for (uint16_t i = 0; i < number_of_unknowns; i++){
+        actual_p[i] = actual_r[i];
+    }
+    //k = 0
+    uint16_t k = 0;
+    float *future_x = new float[number_of_unknowns];
+    float *future_r = new float[number_of_unknowns];
+    float *future_p = new float[number_of_unknowns];
+    //Repeat
+    while (true){
+        float a;
+        float B;
+        //  a_k = <r_k,r_k>/<p_k,p_k>A
+        float *actual_Ap = multiply_sparse_matrix_with_column_matrix(actual_p);
+        float numerator = 0.0F;
+        float denominator = 0.0F;
+        for (uint16_t i = 0; i < number_of_unknowns; i++){
+            numerator += actual_r[i]*actual_r[i];
+            denominator += actual_p[i]*actual_Ap[i];
+        }
+        a = numerator/denominator;
+        //  x_(k+1) = x_k + a_k*p_k
+        for (uint16_t i = 0; i < number_of_unknowns; i++){
+            future_x[i] = actual_x[i] + a*actual_p[i];
+        }
+        //  r_(k+1) = r_k - a_k*A*p_k
+        for (uint16_t i = 0; i < number_of_unknowns; i++){
+            future_r[i] = actual_r[i] - a*actual_Ap[i];
+        }
+        delete [] actual_Ap;
+        //  Exit if r_(k+1) is small
+        float normalized_total_error = 0.0F;
+        for (uint16_t i = 0; i < number_of_unknowns; i++){
+            normalized_total_error += future_r[i]*future_r[i];
+        }
+        normalized_total_error = normalized_total_error/float(number_of_unknowns);
+        if ((normalized_total_error < 0.0000005F) || (k >= (number_of_unknowns-1))){
+            delete [] actual_x;
+            delete [] actual_r;
+            delete [] actual_p;
+            delete [] future_r;
+            delete [] future_p;
+            return future_x;
+        }
+        //  B_k = <r_(k+1), r_(k+1)>/<r_k, r_k>
+        numerator = 0.0F;
+        denominator = 0.0F;
+        for (uint16_t i = 0; i < number_of_unknowns; i++){
+            numerator += future_r[i]*future_r[i];
+            denominator += actual_r[i]*actual_r[i];
+        }
+        B = numerator/denominator;
+        //  p_(k+1) = r_(k+1) + B_k*p_k
+        for (uint16_t i = 0; i < number_of_unknowns; i++){
+            future_p[i] = future_r[i]+B*actual_p[i];
+        }
+        k++;
+        //Preparing next iteration.
+        float *temp;
+        temp = actual_x;
+        actual_x = future_x;
+        future_x = temp;
+        temp = actual_r;
+        actual_r = future_r;
+        future_r = temp;
+        temp = actual_p;
+        actual_p = future_p;
+        future_p = temp;
+    }
+}
+
+float *MatrixSolver::multiply_sparse_matrix_with_column_matrix(float *column_matrix){
+    float *result_matrix = new float[number_of_unknowns];
+    for (uint16_t i = 0; i < number_of_unknowns; i++){
+        result_matrix[i] = 0.0F;
+        uint16_t actual_row_occupation = row_occupation[i];
+        for (uint16_t j = 0; j < actual_row_occupation; j++){
+            result_matrix[i] += matrix_values[i][j]*column_matrix[matrix_column_positions[i][j]];
+        }
+    }
+    return result_matrix;
 }
 
 HexBoard::HexBoard(uint16_t side_size_parameter){
@@ -756,19 +897,19 @@ float HexBoard::evaluation_from_red_perspective(){
 }
 
 uint16_t solve_matrix(float **matrix, uint16_t total_number_of_unknowns, uint16_t central_diagonal_width){
-    for(uint16_t i = 0; i < total_number_of_unknowns; i++){
-        std::cout << "[" << matrix[i][0] ;
-        for(uint16_t j=1; j<total_number_of_unknowns; j++){
-            std::cout << "," << matrix[i][j] ;
-        }
-        std::cout << "],";
-    }
-    std::cout << std::endl;
-    std::cout << "[";
-    for(uint16_t i = 0; i < total_number_of_unknowns; i++){
-        std::cout << matrix[i][total_number_of_unknowns] << ",";
-    }
-    std::cout << std::endl;
+//    for(uint16_t i = 0; i < total_number_of_unknowns; i++){
+//        std::cout << "[" << matrix[i][0] ;
+//        for(uint16_t j=1; j<total_number_of_unknowns; j++){
+//            std::cout << "," << matrix[i][j] ;
+//        }
+//        std::cout << "],";
+//    }
+//    std::cout << std::endl;
+//    std::cout << "[";
+//    for(uint16_t i = 0; i < total_number_of_unknowns; i++){
+//        std::cout << matrix[i][total_number_of_unknowns] << ",";
+//    }
+//    std::cout << std::endl;
     float epsilon = 0.00000001F;
     for (uint16_t i=0;i<total_number_of_unknowns-1;i++){
         uint16_t optimization_big_diagonal = i+central_diagonal_width;
